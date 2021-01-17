911f640b29b8f8e17850ac00e57804cb8c31e5af
Delebrith <paulina.szwed.1996@gmail.com>
2021-01-17 15:58:29 +0100

    add visibility changes according to languages

--- scripts/viewmodel/ViewModel.py
+++ scripts/viewmodel/ViewModel.py
@@ -1,4 +1,5 @@
 import logging
+import random
 
 import networkx as nx
 
@@ -11,6 +12,9 @@ class ViewModel:
         self.model = model
         self.link = None
         self.network = None
+        self.left_nodes = []
+        self.colors = []
+        self.right_nodes = []
         self.available_languages = []
         self.selected_languages = []
         self.timeline_values = []
@@ -23,13 +27,11 @@ class ViewModel:
 
     async def update_link(self):
         logging.debug("Update link")
-        # self.link = link
-        #         read network
         print(self.link)
         await self.model.get_article_data(title="Bitwa pod Cedynią")
-        self.network = self.model.network
-        print(self.network)
-        self.available_languages = ["pl", "en", "de", "fr", "cz"]
+        self._update_network()
+        self.colors = ["#%06x" % random.randint(0, 0xFFFFFF) for node in self.left_nodes]
+        self.available_languages = [str(node).split("__")[1] for node in self.left_nodes]
         self.selected_languages = self.available_languages
         self.timeline_values = ["2019-01-01", "2020-12-31"]
         self.selected_timeline_value = self.timeline_values[0]
@@ -38,8 +40,8 @@ class ViewModel:
         return link == "wiki"
 
     def update_selected_languages(self, selected):
-        self.selected_languages = selected
         print("selected languages:", selected)
+        self.selected_languages = selected
         # update network
 
     def update_analysis_mode(self, selected):
@@ -47,17 +49,14 @@ class ViewModel:
         print(selected)
         # update network
 
-    def get_network(self):
-        G = self.model.network
-        return G, nx.bipartite.sets(G)[0], nx.bipartite.sets(G)[1]
-
-    def get_nodes_names(self):
-        return ["art %d" % i for i in range(0, len(self.network))]
-
-    def get_nodes_fragments(self):
-        return ["fragment %d" % i for i in range(0, len(self.network))]
-
     def update_timeline_value(self, selected):
         print(selected)
         self.selected_timeline_value = selected
         #         update network
+
+    def _update_network(self):
+        self.network = self.model.network
+        self.left_nodes, self.right_nodes = (
+            [node for node in self.network if "__" in node],
+            [node for node in self.network if "__" not in node],
+        )
--- scripts/wikilanggraph/logging.py
+++ scripts/wikilanggraph/logging.py
@@ -10,6 +10,6 @@ import yaml
 
 def enable_logging(root_path: str = ".") -> None:
     with suppress(FileNotFoundError):
-        path = os.path.join(os.path.realpath(filename=root_path), "logging.yml")
+        path = os.path.join(os.path.realpath(root_path), "logging.yml")
         with open(file=path, mode="r") as stream:
             logging.config.dictConfig(yaml.safe_load(stream))
